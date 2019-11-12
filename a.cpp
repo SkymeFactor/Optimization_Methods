d64d6130166d0d91966cb777fe8d5f1b7e049440
SkymeFactor <sergei.51351@yandex.ru>
2019-11-12 05:46:26 +0300

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,1 +1,9 @@
 
+#include <iostream>
+
+void GetInfo () {
+    std::string project_version = "1.0.0";
+    std::string project_date = "01.10.2019";
+    std::cout << project_version;
+    std::cout << project_date;
+}
