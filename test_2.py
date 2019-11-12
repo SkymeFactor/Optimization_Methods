bcb75cb35b6b4cc9ab46455a56a20043bab8666d
Skyme <sergei.51351@yandex.ru>
2019-11-12 09:14:28 +0300

    circle2 commit

--- test_2.py
+++ test_2.py
@@ -1,12 +1,12 @@
 import unittest
-import golden_section as g
+import dichotomy as d
 import math
 
 class TestMethods(unittest.TestCase):
     def setUp(self):
         print("Test started.")
     def TestPassed(self):
-        self.assertAlmostEqual(g.golden_sec(0.000001, -(math.pi / 2), math.pi / 2), -1.57079600431)
+        self.assertAlmostEqual(d.half_divide_method(0.000001, -(math.pi / 2), math.pi / 2), -1.57079600431)
         print("OK.")
     def tearDown(self):
         print("Test ended.")
